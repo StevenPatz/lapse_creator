4c4c5ead3cf5955bfb21aa761a245d09ef0a5c7a
StevenPatz <steven.patz@gmail.com>
2013-01-04 16:21:06 -0500

    Added logic to make sure there is something on the command line Part 2

--- lapse_creator.sh
+++ lapse_creator.sh
@@ -1,5 +1,7 @@
-# If there are just two files, run this script with the files on the command line.
-# TODO make more robust
+if [ $# != 0 ];  then
+
+
+
 
 date=`date +%Y%m%d` 
 #date="2012-07-27" 
@@ -24,3 +26,4 @@ ffmpeg -threads 2 -i $1 -r 30000/1001 -b 1M -bt 3M -vcodec libx264 -pass 1 -acod
 ffmpeg -threads 2 -i $2 -r 30000/1001 -b 1M -bt 3M -vcodec libx264 -pass 1 -acodec libfaac -ac 1 -ar 48000 -ab 56k roadtrip_from_$4_sound_$date.mp4
 #ffmpeg -threads 2 -i $3 -r 30000/1001 -b 2M -bt 4M -vcodec libx264 -pass 1 -acodec libfaac -ac 1 -ar 48000 -ab 56k roadtrip_from_$4_sound_part2_$date.mp4
 
+fi
